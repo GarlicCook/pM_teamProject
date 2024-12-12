ffc556216a8cc3e8bb48c571438b552d5f83bcfd
Jinkun Koh <kjk0719@hanyang.ac.kr>
2024-12-12 18:10:16 +0900

    Number_Input_PuzzleFrame.java
    PuzzleFrame Constructor에서 파라미터로 SlidePuzzleBoard b, int s(size)를 받아 크기 변경

--- src/PuzzleFrame.java
+++ src/PuzzleFrame.java
@@ -2,50 +2,43 @@ import javax.swing.*;
 import java.awt.*;
 
 public class PuzzleFrame extends JFrame {
-	
-	private SlidePuzzleBoard board;
-	private PuzzleButton[][] button_board;
-	
-	public PuzzleFrame(SlidePuzzleBoard b) {
-		board = b;
-		button_board = new PuzzleButton[board.getSize()][board.getSize()];
-		Container cp = getContentPane();
-		cp.setLayout(new BorderLayout());
-		JPanel p1 = new JPanel((new FlowLayout()));
-		JPanel p2 = new JPanel((new GridLayout(board.getSize(), board.getSize())));
+    private SlidePuzzleBoard board;
+    private PuzzleButton[][] button_board;
+    private int size;
+    public PuzzleFrame(SlidePuzzleBoard b, int s) {
+        size = s;
+        board = b;
+        button_board = new PuzzleButton[size][size];
+        Container cp = getContentPane();
+        cp.setLayout(new BorderLayout());
+        JPanel boardPanel = new JPanel(new GridLayout(size, size));
+        for (int row = 0; row < button_board.length; row++)
+            for (int col = 0; col < button_board.length; col++) {
+                button_board[row][col] = new PuzzleButton(board, this);
+                boardPanel.add(button_board[row][col]);
+            }
+        update();
+        setTitle("Slide Puzzle"); setSize(100 + 50 * size, 100 + 50 * size); setVisible(true);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        StartButton startButton = new StartButton(board, this);
+        cp.add(startButton, BorderLayout.NORTH);
+        cp.add(boardPanel, BorderLayout.CENTER);
 
-		//startButton 생성
-		p1.add(new StartButton(board, this));
-		cp.add(p1, BorderLayout.NORTH);
+    }
+    public void update() {
+        PuzzlePiece pp;
+        for (int row = 0; row < button_board.length; row++)
+            for (int col = 0; col < button_board.length; col++) {
+                pp = board.getPuzzlePiece(row, col);
+                if (pp != null)
+                    button_board[row][col].setText(Integer.toString(pp.faceValue()));
+                else button_board[row][col].setText("");
+            }
+        if (board.gameOver()) finish();
+    }
 
-		//puzzleButton 생성
-		for (int row = 0; row < button_board.length; row++)
-			for (int col = 0; col < button_board.length; col++) {
-				button_board[row][col] = new PuzzleButton(board,this);
-				p2.add(button_board[row][col]);
-			}
-		cp.add(p2, BorderLayout.CENTER);
-
-
-		update();
-		setTitle("Slide Puzzle");
-		setSize(400,400);
-		setVisible(true);
-		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-	}
-	
-	public void update() {
-		PuzzlePiece pp;
-		for (int row = 0; row < button_board.length; row++)
-			for (int col = 0; col < button_board.length; col++) {
-				pp = board.getPuzzlePiece(row, col);
-				if (pp != null)
-					button_board[row][col].setText(Integer.toString(pp.faceValue()));
-				else
-					button_board[row][col].setText("");
-
-        }
-		if(!board.gameOn() && (board.gameCount() != 1))
-			button_board[board.getSize()-1][board.getSize()-1].setText("Done");
+    /** finish - 퍼즐 게임 종료를 표시함 */
+    public void finish() {
+        button_board[size - 1][size - 1].setText("Done");
     }
 }

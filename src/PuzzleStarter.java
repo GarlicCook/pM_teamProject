55c3e045c9daab3c825cbb01fec9eacb4783e4ce
LaLa4978 <159722251+LaLa4978@users.noreply.github.com>
2024-12-13 17:50:43 +0900

    Update PuzzleStarter.java

--- src/PuzzleStarter.java
+++ src/PuzzleStarter.java
@@ -1,5 +1,15 @@
+import javax.swing.*;
+
 public class PuzzleStarter {
     public static void main(String[] args) {
-        new PuzzleFrame(new SlidePuzzleBoard(1));
+        JOptionPane.showMessageDialog(null, "게임을 시작합니다.");
+        while(true) {
+            int size = Integer.parseInt(JOptionPane.showInputDialog("게임판의 크기를 입력해주세요.(2~9) "));
+            if (size > 1 && size <= 9) {
+                new PuzzleFrame(new SlidePuzzleBoard(size));
+                break;
+            } else {
+                JOptionPane.showMessageDialog(null, "크기를 다시 입력해주세요.");}
+        }
     }
 }

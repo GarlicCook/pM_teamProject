1837ef77d122f9dc645b401428edf96df928bb12
Jinkun Koh <kjk0719@hanyang.ac.kr>
2024-12-14 00:27:32 +0900

    PuzzleFrame_GUI.java
    SlidePuzzleBoard의 그래픽 변경

--- src/PuzzleFrame.java
+++ src/PuzzleFrame.java
@@ -2,43 +2,67 @@ import javax.swing.*;
 import java.awt.*;
 
 public class PuzzleFrame extends JFrame {
+
     private SlidePuzzleBoard board;
     private PuzzleButton[][] button_board;
-    private int size;
-    public PuzzleFrame(SlidePuzzleBoard b, int s) {
-        size = s;
+
+    public PuzzleFrame(SlidePuzzleBoard b) {
         board = b;
-        button_board = new PuzzleButton[size][size];
+        button_board = new PuzzleButton[board.getSize()][board.getSize()];
         Container cp = getContentPane();
-        cp.setLayout(new BorderLayout());
-        JPanel boardPanel = new JPanel(new GridLayout(size, size));
-        for (int row = 0; row < button_board.length; row++)
-            for (int col = 0; col < button_board.length; col++) {
-                button_board[row][col] = new PuzzleButton(board, this);
-                boardPanel.add(button_board[row][col]);
+        cp.setLayout(null);
+
+        JPanel p1 = new JPanel(new FlowLayout());
+        p1.setBounds(0, 0, board.getSize() * 90 + 20, 30);
+        StartButton start_button = new StartButton(board, this);
+        start_button.setBackground(Color.GRAY);
+        p1.add(start_button);
+        cp.add(p1);
+
+        final int buttonSize = 80;
+        final int blank = 10;
+
+        for (int row = 0; row < board.getSize(); row++) {
+            for (int col = 0; col < board.getSize(); col++) {
+                PuzzlePiece pp = board.getPuzzlePiece(row, col);
+                int x = 10 + col * (buttonSize + blank);
+                int y = 40 + row * (buttonSize + blank);
+                PuzzleButton button = new PuzzleButton(board, this);
+                button.setBackground(Color.WHITE);
+                button.setBounds(x, y, buttonSize, buttonSize);
+                if (pp != null) {
+                    button.setText(Integer.toString(pp.faceValue()));
+                } else {
+                    button.setText("");
+                }
+                cp.add(button);
+                button_board[row][col] = button;
             }
-        update();
-        setTitle("Slide Puzzle"); setSize(100 + 50 * size, 100 + 50 * size); setVisible(true);
+        }
+
+        setTitle("Slide Puzzle");
+        setSize(board.getSize() * 90 + 20, board.getSize() * 90 + 70);
+        setVisible(true);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        StartButton startButton = new StartButton(board, this);
-        cp.add(startButton, BorderLayout.NORTH);
-        cp.add(boardPanel, BorderLayout.CENTER);
-
     }
+
     public void update() {
-        PuzzlePiece pp;
-        for (int row = 0; row < button_board.length; row++)
-            for (int col = 0; col < button_board.length; col++) {
-                pp = board.getPuzzlePiece(row, col);
-                if (pp != null)
+        for (int row = 0; row < board.getSize(); row++) {
+            for (int col = 0; col < board.getSize(); col++) {
+                PuzzlePiece pp = board.getPuzzlePiece(row, col);
+                if (pp != null) {
                     button_board[row][col].setText(Integer.toString(pp.faceValue()));
-                else button_board[row][col].setText("");
+                } else {
+                    button_board[row][col].setText("");
+                }
             }
-        if (board.gameOver()) finish();
+        }
+        if (!board.gameOn() && (board.gameCount() != 1)) {
+            button_board[board.getSize() - 1][board.getSize() - 1].setText("Done");
+        }
     }
 
-    /** finish - 퍼즐 게임 종료를 표시함 */
-    public void finish() {
-        button_board[size - 1][size - 1].setText("Done");
+    public void movingButton() {
+
     }
 }
